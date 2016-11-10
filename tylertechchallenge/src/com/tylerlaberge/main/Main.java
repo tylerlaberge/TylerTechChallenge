ab8f849adc36e5b2b2f98495f8da9aa9150f3b61
tylerlaberge <tyler.laberge@maine.edu>
2016-11-09 22:04:41 -0500

    wrote unit test for main output

--- tylertechchallenge/src/com/tylerlaberge/main/Main.java
+++ tylertechchallenge/src/com/tylerlaberge/main/Main.java
@@ -1,10 +1,22 @@
 package com.tylerlaberge.main;
 
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-        for (String arg : args) {
-            System.out.println(arg);
+    public static void main(String[] args) throws IOException {
+        String input_file_path = args[0];
+        String output_file_path = args[1];
+        try (
+                BufferedReader reader = new BufferedReader(new FileReader(input_file_path));
+                Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(output_file_path), "utf-8"));
+        ) {
+            String line = reader.readLine();
+            while(line !=null)
+            {
+                writer.write(line);
+                line = reader.readLine();
+            }
         }
     }
 }
--- tylertechchallenge/src/tests/MainTest.java
+++ tylertechchallenge/src/tests/MainTest.java
@@ -1,41 +1,54 @@
 package tests;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+import org.junit.*;
 
 import com.tylerlaberge.main.Main;
+import org.junit.rules.TemporaryFolder;
+
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class MainTest {
 
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
-    private final String[] args = {"foo", "bar"};
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Before
     public void setUp() throws Exception {
-        System.setOut(new PrintStream(outContent));
-        System.setErr(new PrintStream(errContent));
     }
 
     @After
     public void tearDown() throws Exception {
-        System.setOut(null);
-        System.setErr(null);
     }
 
     @Test
     public void main() throws Exception {
-        Main.main(this.args);
 
-        String output = this.outContent.toString().replaceAll("\n", "").replaceAll("\r", "");
+        String[] args = new String[2];
+        URL input_file_url = MainTest.class.getResource("input_files/input1.txt");
+        URL output_file_url = MainTest.class.getResource("output_files/output1.txt");
+        Path input_file_path = Paths.get(input_file_url.toURI());
+        Path output_file_path = Paths.get(output_file_url.toURI());
 
-        assertEquals("foobar", output);
-        assertEquals("", this.errContent.toString());
+        args[0] = input_file_path.toString();
+        args[1] = this.folder.getRoot() + "/output.txt";
+
+        Main.main(args);
+
+        Path expected_output_file_path = FileSystems.getDefault().getPath(output_file_path.toString());
+        Path actual_output_path = FileSystems.getDefault().getPath(args[1]);
+
+        List<String> expected_lines = Files.readAllLines(expected_output_file_path, StandardCharsets.UTF_8);
+        List<String> actual_lines = Files.readAllLines(actual_output_path, StandardCharsets.UTF_8);
+
+        assertEquals(expected_lines, actual_lines);
     }
-
 }
